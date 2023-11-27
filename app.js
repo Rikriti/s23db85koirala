08f47454319967e842c0bf1baca0705fb558f20d
Rikriti Koirala <rikritikoirala@gmail.com>
2023-11-26 19:28:33 -0600

    some changes made for part 4

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var Plants = require("./models/plants");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+var Account = require('./models/account');
+
+
 
 require("dotenv").config();
 const connectionString = process.env.MONGO_CON;
@@ -87,6 +92,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -96,6 +108,10 @@ app.use("/board", boardRouter);
 app.use("/choose", ChooseRouter);
 app.use("/resource", resourceRouter);
 app.use("/plants", pl);
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -112,5 +128,27 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+function(username, password, done) {
+Account.findOne({ username: username })
+.then(function (user){
+if (err) { return done(err); }
+if (!user) {
+return done(null, false, { message: 'Incorrect username.' });
+}
+if (!user.validPassword(password)) {
+return done(null, false, { message: 'Incorrect password.' });
+}
+return done(null, user);
+})
+.catch(function(err){
+return done(err)
+})
+})
+);
 
 module.exports = app;
